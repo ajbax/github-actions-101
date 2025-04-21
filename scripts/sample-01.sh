8872c0755547f6d8e9aacf69ba5cec38d9f86164
Adi Bal-Mayel <abalmayel@gmail.com>
2025-04-20 22:35:50 -0400

    Workflow running script

---
+++ scripts/sample-01.sh
@@ -0,0 +1,5 @@
+echo "#### SCRIPT # 1 ####"
+sleep 5
+echo "Start of the script"
+sleep 10
+echo "End of the script"
---
+++ scripts/sample-02.sh
@@ -0,0 +1,5 @@
+echo "#### SCRIPT # 2 ####"
+sleep 1
+echo "Starting script number 2"
+sleep 10
+echo "End of the script"
